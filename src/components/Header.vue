7556ac699a5a7d66a4ef3455a979538d766c3468
sapiens19 <sapiens19@pop-os.localdomain>
2024-05-27 16:42:33 -0300

    Created gloval styles and Header component HTML and style

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    <strong>SpaceTasks</strong>
+  </header>
+</template>
+
+<style>
+  .header {
+    background: var(--gray-700);
+    display: flex;
+    justify-content: center;
+    padding: 4rem 0;
+  }
+
+  .header strong {
+    display: flex;
+    color: var(--blue);
+  }
+
+</style>
